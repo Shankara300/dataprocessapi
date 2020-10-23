28fe9a060359e13ff8c4a95bccca80c8bb187890
JDA\1022207 <Shankara.PrasadSU@blueyonder.com>
2020-10-23 10:26:03 +0530

    Added class comment

--- src/main/java/com/infrrd/api/service/ReentrantLockSingleton.java
+++ src/main/java/com/infrrd/api/service/ReentrantLockSingleton.java
@@ -1,7 +1,9 @@
 package com.infrrd.api.service;
 
 import java.util.concurrent.locks.ReentrantLock;
-
+/*
+ * Gives singleton object for ReentrantLock.
+ */
 public class ReentrantLockSingleton {
 	private static ReentrantLock singleton = null;
 
@@ -11,7 +13,7 @@ public class ReentrantLockSingleton {
 
     public static ReentrantLock getInstance() {
         if (singleton == null) {
-            synchronized (ReentrantLockSingleton .class) {
+            synchronized (ReentrantLockSingleton.class) {
                 if (singleton == null) {
                 	singleton = new ReentrantLock();
                 }
